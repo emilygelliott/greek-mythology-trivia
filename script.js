dbcc1c62fa89c4d43bd2b0f62ab61575025ffc71
Emily Elliott <emily.elliott2001@gmail.com>
2025-12-02 11:45:17 -0330

    Added 15 questions in JS

---
+++ script.js
@@ -0,0 +1,84 @@
+let questions = [
+    {
+        question: "Who is the king of the greek gods?",
+        answers: ["Zeus", "Poseidon", "Apollo", "Hades"],
+        correct: "Zeus"
+    },
+    {
+        question: "What is the name of the goddess of wisdom and war strategy?",
+        answers: ["Helena", "Athena", "Persephone", "Hera"],
+        correct: "Athena"
+    },
+    {
+        question: "Who is the god of the sea?",
+        answers: ["Hades", "Scylla", "Odysseus","Poseidon"],
+        correct: "Poseidon"
+    },
+    {
+        question: "What is the name of the messenger god with winged sandals?",
+        answers: ["Apollo", "Hermes", "Hera", "Calypso"],
+        correct: "Hermes"
+    },
+    {
+        question: "Who is the king of the underworld?",
+        answers: ["Zeus", "Poseidon", "Achilles", "Hades"],
+        correct: "Hades"
+    },
+    {
+        question: "What creature is half-man, half-bull?",
+        answers: ["Minotaur", "Scylla", "", "Centaur", "Cyclops"],
+        correct: "Minotaur"
+    },
+    {
+        question: "Which god stole fire from the gods and gave it to humans?",
+        answers: ["Hermes", "Ares", "Hephaestus", "Prometheus"],
+        correct: "Prometheus"
+    },
+    {
+        question: "Which goddess turned Arachne into a spider?",
+        answers: ["Artemis", "Hera", "Demeter", "Athena"],
+        correct: "Athena"
+    },
+    {
+        question: "What is the name of the creature that has the head of a woman and snakes for hair?",
+        answers: ["Medusa", "Sirens", "Scylla", "Echidna"],
+        correct: "Medusa"
+    },
+    {
+        question: "Which hero's weakness was his heel?",
+        answers: ["Hercules", "Theseus", "Achilles", "Odysseus"],
+        correct: "Achilles"
+    },
+    {
+        question: "Who is the goddess of love and beauty?",
+        answers: ["Persephone", "Aphrodite", "Hera", "Athena"],
+        correct: "Aphrodite"
+    },
+    {
+        question: "Which titan holds up the sky?",
+        answers: ["Zeus", "Atlas", "Kronos", "Prometheus"],
+        correct: "Atlas"
+    },
+    {
+        question: "What monster does Odysseus blind in a cave?",
+        answers: ["Cyclops", "Scylla", "Siren", "Charybdis"],
+        correct: "Cyclops"
+    },
+    {
+        question: "How many heads is Scylla described as having?",
+        answers: ["Six", "Three", "Nine", "Seven"],
+        correct: "Six"
+    },
+    {
+        question: "Who flew too close to the sun, causing his wax wings to melt?",
+        answers: ["Hermes", "Helios", "Apollo", "Icarus"],
+        correct: "Icarus"
+    }
+]
+
+const question = document.getElementById("question");
+const answers = document.getElementById("answers");
+const start = document.getElementById("start");
+const next = document.getElementById("next");
+const score = document.getElementById("score");
+
